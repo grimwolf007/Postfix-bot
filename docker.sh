7e07044c3c3f11c34036479a44da9936423661df
grimwolf007 <digimon6jrd@gmail.com>
2022-10-03 22:32:11 -0500

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,8 @@
+docker run -e POSTFIXADMIN_DB_TYPE=mysqli \
+           -e POSTFIXADMIN_DB_HOST=whatever \
+           -e POSTFIXADMIN_DB_USER=user \
+           -e POSTFIXADMIN_DB_PASSWORD=topsecret \
+           -e POSTFIXADMIN_DB_NAME=postfixadmin \
+           --name some-postfixadmin \
+           -p 8000:80
+        postfixadmin
